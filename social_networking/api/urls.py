7c3c398861d1030063c4cfcd20b4e35a73c0e9c0
paras-gill <paras_gill@outlook.com>
2024-06-09 14:48:02 +0530

    Login working

---
+++ social_networking/api/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from networking.views import LoginView, RegisterView, UserSearchAPIView,send_friend_request, respond_friend_request, list_friends, list_pending_friend_requests
+
+urlpatterns = [
+    path('register/', RegisterView.as_view()),
+]
--- social_networking/networking/views.py
+++ social_networking/networking/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model, authenticate  
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q
+from django.utils import timezone
 
-# Create your views here.
+from rest_framework import generics, permissions, status
+from rest_framework.decorators import api_view, permission_classes, authentication_classes, throttle_classes 
+from rest_framework.response import Response 
+from rest_framework.views import APIView
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.throttling import UserRateThrottle
+from rest_framework.throttling import UserRateThrottle
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+from .serializers import CustomUserSerializer, LoginSerializer, FriendRequestSerializer
+from .models import FriendRequest
+
+User = get_user_model()
+
+
+
+# View for user registration
+class RegisterView(APIView):
+    permission_classes = [permissions.AllowAny]  # No authentication required
+    def post(self, request):
+        serializer = CustomUserSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "User created successfully","data": serializer.data}, status=status.HTTP_201_CREATED)    
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
