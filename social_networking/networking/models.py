ae1e6f3b481264ec0406d710fede2624ab933e40
paras-gill <paras_gill@outlook.com>
2024-06-10 03:39:24 +0530

    CustomUserManager class added

--- social_networking/networking/models.py
+++ social_networking/networking/models.py
@@ -1,5 +1,26 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, name, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, name, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, name, password, **extra_fields)
 
 
 # Custom user model extending Django's AbstractUser
@@ -9,6 +30,8 @@ class CustomUser(AbstractUser):
     name = models.CharField(max_length=100)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name']
+
+    objects = CustomUserManager()
 
     def __str__(self):
         return self.email
@@ -22,3 +45,4 @@ class FriendRequest(models.Model):
     pending_status = models.BooleanField(default=True)     # True if request is pending, False if responded
     response_status = models.BooleanField(null=True, blank=True)      # True for accepted, False for rejected, Null if pending
     responded_at = models.DateTimeField(null=True, blank=True)       # Time when the request was responded
+
